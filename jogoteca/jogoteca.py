7935665903e5a19aa33eee5f7e50ebcf26837d7f
Jordi Ribeiro <jordi.ribeiro.fonseca@gmail.com>
2023-02-16 16:50:19 -0300

    [Curso Alura]Usando estrutura de repeticao python no HTML

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def  __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    jogo1= Jogo('Tetris','Arcade','Atari')
+    jogo2 =Jogo('Crash','ForFun','PS3')
+    jogo3 = Jogo('Skyrim','RPG','PS4')
+    lista = [jogo1,jogo2,jogo3]
+    return render_template('lista.html',titulo='Jogos',jogos=lista)
 
 app.run()
