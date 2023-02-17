1c0a0dd35a400ac267b40f1b77872692d1adace6
Jordi Ribeiro <jordi.ribeiro.fonseca@gmail.com>
2023-02-17 00:28:21 -0300

    [Curso Alura] Criando a rota /criar e ajustando o form

--- jogoteca/jogoteca.py
+++ jogoteca/jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def  __init__(self, nome, categoria, console):
@@ -6,14 +6,29 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo0= Jogo('Tetris','Arcade','Atari')
+jogo1 =Jogo('Crash','ForFun','PS3')
+jogo2 = Jogo('Skyrim','RPG','PS4')
+lista = [jogo0,jogo1,jogo2]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    jogo1= Jogo('Tetris','Arcade','Atari')
-    jogo2 =Jogo('Crash','ForFun','PS3')
-    jogo3 = Jogo('Skyrim','RPG','PS4')
-    lista = [jogo1,jogo2,jogo3]
+
+    return render_template('lista.html',titulo='Jogos',jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome,categoria,console)
+    lista.append(jogo)
     return render_template('lista.html',titulo='Jogos',jogos=lista)
 
 app.run()
